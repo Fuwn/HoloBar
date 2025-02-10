fcddfd8b8daf543be908ccf69b751788dee2b13f
Fuwn <contact@fuwn.me>
2025-02-10 04:59:47 -0800

    feat(HoloBarApp): automatic refresh on date change

--- HoloBar/HoloBarApp.swift
+++ HoloBar/HoloBarApp.swift
@@ -6,29 +6,44 @@ struct HoloBarApp: App {
 
     var body: some Scene {
         MenuBarExtra {
-            if fetcher.characters.isEmpty {
-                Text("Loading …")
-            } else {
-                ForEach(fetcher.characters) { character in
-                    Button(character.name) {
-                        NSWorkspace.shared.open(character.profileURL)
+            VStack {
+                if fetcher.characters.isEmpty {
+                    Text("Loading …")
+                } else {
+                    ForEach(fetcher.characters) { character in
+                        Button(character.name) {
+                            NSWorkspace.shared.open(character.profileURL)
+                        }
                     }
                 }
-            }
 
-            Divider()
-            Button("Refresh") {
-                let today = Calendar.current.dateComponents([.month, .day], from: Date())
-
-                if let month = today.month, let day = today.day {
-                    fetcher.fetchCharacters(for: month, day: day)
+                Divider()
+                #if DEBUG
+                    Button("Simulate Day Change") {
+                        NotificationCenter.default.post(name: .NSCalendarDayChanged, object: nil)
+                    }
+                #endif
+                Button("Refresh") {
+                    refreshCharacters()
+                }
+                Button("Quit") {
+                    NSApplication.shared.terminate(nil)
                 }
             }
-            Button("Quit") {
-                NSApplication.shared.terminate(nil)
+            .onReceive(NotificationCenter.default.publisher(for: .NSCalendarDayChanged)) { _ in
+                refreshCharacters()
             }
         } label: {
             Text("HL")
+        }
+    }
+
+    private func refreshCharacters() {
+        let today = Calendar.current.dateComponents([.month, .day], from: Date())
+
+        if let month = today.month, let day = today.day {
+            fetcher.characters.removeAll()
+            fetcher.fetchCharacters(for: month, day: day)
         }
     }
 }
